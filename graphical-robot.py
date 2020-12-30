741a4a2d7561c8fc94dd0dbc8c03f147130ef8ed
Larry6oy <peng.larry@outlook.com>
2020-12-30 16:49:20 -0500

    Here we go! the Robot is built!

--- graphical-robot.py
+++ graphical-robot.py
@@ -1,4 +1,5 @@
 import turtle as t;
+t.hideturtle()
 def rectangle(horizontal, vertical, color):
     
     t.pendown()
@@ -26,3 +27,33 @@ rectangle(15, 100, 'grey')
 
 t.goto(-55, -50)
 rectangle(-15, 100, 'grey')
+#body
+t.goto(-90, 100)
+rectangle(100, 150, 'red')
+#arms
+t.goto(-150,70)
+rectangle(60, 15, 'grey')
+t.goto(-150, 110)
+rectangle(15, 40, 'grey')
+
+
+t.goto(10, 70)
+rectangle(60, 15, 'grey')
+t.goto(55, 110)
+rectangle(15, 40, 'grey')
+#neck
+t.goto(-50, 120)
+rectangle(15, 20, 'grey')
+#head
+t.goto(-85, 170)
+rectangle(80, 50, 'red')
+#eyes
+t.goto(-60, 160)
+rectangle(30, 10, 'white')
+t.goto(-40, 155)
+rectangle(5, 5, 'black')
+t.goto(-55, 155)
+rectangle(5, 5, 'black')
+#mouth
+t.goto(-65, 135)
+rectangle(40, 5, 'black')
